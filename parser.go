3a39a301fd689bc2554b6771f91eb2d00dd317a9
Claudson Oliveira <claudsonweb@gmail.com>
2014-08-01 01:05:35 -0300

    Initial commit
    ┬┌┐┌┬┌┬┐┬┌─┐┬    ┌─┐┌─┐┌┬┐┌┬┐┬┌┬┐
    │││││ │ │├─┤│    │  │ ││││││││ │
    ┴┘└┘┴ ┴ ┴┴ ┴┴─┘  └─┘└─┘┴ ┴┴ ┴┴ ┴...

---
+++ parser.go
@@ -0,0 +1,87 @@
+package parser
+
+import (
+	"strings"
+	"strconv"
+	"errors"	
+)
+
+type Parser struct {
+	header *Header
+	contentRaw string
+	body string
+}
+
+type Header struct {
+	name string
+	height int 
+	numberOfLines int 
+	positions map[rune]int
+	alphabet string
+}
+
+func New(fileContent string) (*Parser) {
+	p := new(Parser)
+	p.contentRaw = fileContent
+	p.header = generateHeader(p.contentRaw)
+	lines := strings.Split(p.contentRaw, "\n")
+	
+	if len(lines) > p.header.numberOfLines + 1 {
+		p.body = strings.Join(lines[p.header.numberOfLines +1:], "\n")
+	}	
+
+	return p
+}
+
+func (p *Parser) getPositions() (map[rune]int) {
+	var positions = make(map[rune]int)
+	for pos, char := range p.header.alphabet {
+		positions[char] = pos + 1
+	}
+
+	return positions
+}
+
+func (p *Parser) getChar(char rune) (string, error) {
+	positions := p.getPositions()
+	position, ok := positions[char]
+	if !ok {
+		return "", errors.New("Char is not found")
+	}
+	
+	contentFiles := strings.Split(p.body, "\n")
+	if p.body == ""{
+		return "", errors.New("Char has not ascii code") 
+	}
+	charRange := contentFiles[(position-1) * p.header.height:(position -1)* p.header.height + p.header.height]
+	charResult := strings.Join(charRange, "\n")
+
+	return charResult, nil
+}
+
+
+func generateHeader(content string) *Header {
+	header := new(Header)
+	header.numberOfLines = 0
+	lines := strings.Split(content, "\n")
+	for _, line := range lines {
+		metadataLine := strings.Split(line, ":")
+		if len(metadataLine) >= 2 {
+			value := strings.Trim(metadataLine[1], " ")
+			switch strings.Trim(metadataLine[0], " ") {
+				case "Name": 
+					header.name = value
+				case "Height" :
+					header.height, _ = strconv.Atoi(value)
+				case "Alphabet":
+					header.alphabet = value
+
+			}
+		}
+		if strings.Index(line, "===") == 0 {
+			break
+		}
+		header.numberOfLines += 1
+	}
+	return header
+}
---
+++ parser_test.go
@@ -0,0 +1,154 @@
+package parser 
+
+import (
+	"testing"
+	"strings"
+)
+
+func TestFontname(t *testing.T) {
+	expected := "Cloud Font"
+	fileContent := `
+Name: Cloud Font 
+	`
+	h := generateHeader(fileContent)
+	found := h.name
+
+	if found != expected {
+		t.Errorf("Expected Font name '%s' and found '%s' ", expected, found)
+	}
+}
+
+func TestFontHeight(t *testing.T) {
+	expected := 5
+	fileContent := `
+Name: Best Font
+Height : 5 
+	`
+	h := generateHeader(fileContent)
+	found := h.height
+
+	if found != expected {
+		t.Errorf("Expected height %d and found height %d ", expected, found)
+	}
+}
+
+func TestEndOfHeader(t *testing.T) {
+	fileContent := `Name: Best Font
+Height : 5 
+======
+Get: lucky! 
+	`
+	expected := 2 
+
+	h := generateHeader(fileContent)
+	found := h.numberOfLines
+	if expected != found {
+		t.Errorf("The header should be %d lines instead of %d", expected, found)
+	}
+}
+
+func TestAValidCharPosition(t *testing.T) {
+	fileContent := `Name: Saturno Font
+Height: 6
+Alphabet: cloud
+========
+`
+	expecteds :=  map[rune]int{'c':1, 'l':2, 'o':3, 'u':4, 'd':5} 
+
+	p := New(fileContent)
+	positions := p.getPositions()
+	if len(expecteds) != len(positions) {
+		t.Errorf("Positions has not the expected length (%d != %d)", len(positions), len(expecteds));
+	}
+	for char, pos := range positions {
+		if expecteds[char] != pos {
+			t.Errorf("Symbol '%s' should be at position %d, it's at %d", string(char), pos, expecteds[char])
+		}
+	}
+}
+
+func TestAValidChar(t *testing.T) {
+	fileContent := `Name: Saturno Font
+Height: 6
+Alphabet: c
+========
+        
+  ____  
+_/ ___\ 
+\  \___ 
+ \___  >
+     \/`
+	expected := 
+`        
+  ____  
+_/ ___\ 
+\  \___ 
+ \___  >
+     \/`
+
+	p := New(fileContent)
+	char, error := p.getChar('c');
+	if error != nil {
+		t.Error(error)
+	}
+	if char != expected {
+		t.Errorf("Expected:\n%s\nFound:\n%s\n", expected, char)
+	}
+}
+
+
+func TestValidsChars(t *testing.T) {
+	fileContent := `Name: Saturno Font
+Height: 6
+Alphabet: cl
+========
+        
+  ____  
+_/ ___\ 
+\  \___ 
+ \___  >
+     \/
+ __ 
+|  |  
+|  |  
+|  |__
+|____/
+      `
+	expected := 
+	`
+  ____  
+_/ ___\ 
+\  \___ 
+ \___  >
+     \/`
+
+	p := New(fileContent)
+	char, error := p.getChar('c');
+	if error != nil {
+		t.Error(error)
+	}
+	if serialize(char) != serialize(expected) {
+		t.Errorf("Expected:\n'%s'\nFound:\n'%s'\n", expected, char)
+	}
+
+	expected = ` __ 
+|  |  
+|  |  
+|  |__
+|____/
+      `
+	char1, error1 := p.getChar('l');
+	if error1 != nil {
+		t.Error(error1)
+	}
+	if serialize(char1) != serialize(expected) {
+		t.Errorf("Expected:\n'%s'\nFound:\n'%s'\n", expected), char1)
+	}
+
+
+}
+
+
+func serialize(char string) string {
+	return strings.Replace(char, " ", "", -1)
+}
