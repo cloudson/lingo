b6cb794765f6e2a3047ea40bd2c0496456d63709
Claudson Oliveira <claudsonweb@gmail.com>
2014-08-05 23:54:34 -0300

    Add methods that print a char

--- symbol/symbol.go
+++ symbol/symbol.go
@@ -1,6 +1,7 @@
 package symbol 
 
 import (
+	"errors"
 	"strings"
 	"unicode/utf8"
 )
@@ -8,18 +9,16 @@ import (
 type Symbol struct {
 	raw string
 	lines []string
-	maxWidth int 
 }
 
 func New(char string) (*Symbol) {
 	s := new(Symbol)
 	s.raw = char
-	s.maxWidth = s.width()
 
 	return s
 }
 
-func (s *Symbol) width() int {
+func (s *Symbol) Width() int {
 	lines := strings.Split(s.raw, "\n")
 	maxWidth := 0 
 	for _, line := range lines {
@@ -34,4 +33,19 @@ func (s *Symbol) width() int {
 
 func (s *Symbol) String() string {
 	return  s.raw
+}
+
+
+func Print(s *Symbol, position int) (string, error) {
+	lines := strings.Split(s.raw, "\n")
+
+	if  position > len(lines) - 1 {
+		return "", errors.New("Trying access inexisting line")
+	}
+
+	lineRaw := lines[position-1]
+	lineResult := lineRaw + strings.Repeat(" ", s.Width() - utf8.RuneCountInString(lineRaw))
+
+
+	return lineResult, nil
 }
--- symbol/symbol_test.go
+++ symbol/symbol_test.go
@@ -23,7 +23,51 @@ func TestMaxWidth(t *testing.T) {
 `
 	expected := 3
 	symbol := New(char)
-	if expected != symbol.maxWidth {
-		t.Errorf("Max width expected %d, found %d", expected, symbol.maxWidth)
+	if expected != symbol.Width() {
+		t.Errorf("Max width expected %d, found %d", expected, symbol.Width())
 	}
 }
+
+func TestPrintOneLineWithMaxChars(t *testing.T) {
+	char := `┌─┐
+│  
+└─┘
+`
+	expected := "└─┘"
+	s := New(char)
+	found, error := Print(s, 3)
+	if error != nil {
+		t.Error(error)
+	}
+	if expected != found {
+		t.Errorf("Expected:\n'%s'\nFound:\n'%s'\n", expected, found)
+	} 
+}
+
+func TestPrintLineOutOfRange(t *testing.T) {
+	char := `┌─┐
+│  
+└─┘`
+	
+	s:= New(char)
+	_, error:= Print(s, 5)
+	if error == nil {
+		t.Error("Should throw error when print out of range")
+	}
+}
+
+func TestPrintLineAddingSpaces(t *testing.T) {
+	char := `┌─┐
+│
+└─┘
+`
+	expected := "│  "
+	s := New(char)
+	found, error := Print(s, 2)
+	if error != nil {
+		t.Error(error)
+	}
+	if expected != found {
+		t.Errorf("Expected:\n'%s'\nFound:\n'%s'\n", expected, found)
+	} 		
+}	
